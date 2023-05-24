a9dd37c3ea394eb1ea8eb11a783c68b2c81ca7ce
Daniyar <127934968+Donny09@users.noreply.github.com>
2023-05-24 13:05:15 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      todoList: [],
+    };
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleCreateTodo = () => {
+    if (this.state.inputValue.trim() === "") {
+      return;
+    }
+  
+    const update = this.state.todoList.concat(this.state.inputValue);
+    this.setState({
+      inputValue: "",
+      todoList: update,
+    });
+  };
+  
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleInputChange}
+            placeholder="Введите задачу"
+          />
+          <button onClick={this.handleCreateTodo}>
+            Сохранить
+          </button>
+        </div>
+        <div>
+          {this.state.todoList.map((todo, index) => (
+            <div key={index}>
+              <h3>{todo}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
